88737fa8a453117c1cf2ba5e0fc33371582d7c42
Alex Barchuk <ax.barchuk@gmail.com>
2019-08-23 23:05:05 +0300

    Added fetching data.

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,64 @@
+import Layout from '../components/Layout';
 import React from "react";
-import Layout from "../components/Layout";
-import Link from "next/link";
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const PostLink = props => (
+const PostLink = ({show}) => (
     <li>
-        <Link href="/p/[id]" as={`/p/${props.id}`}>
-            <a>{props.id}</a>
+        <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
         </Link>
+
+        <style jsx>{`
+        li {
+            list-style: none;
+            margin: 5px 0;
+        }
+        
+        a {
+            text-decoration: none;
+            color: blue;
+            font-family: 'Arial';
+        }
+        
+        a:hover {
+            opacity: 0.6;
+        }
+        `}</style>
     </li>
 );
 
-export default function Blog() {
-    return (
-        <Layout>
-            <h1>My Blog</h1>
-            <ul>
-                <PostLink id="hello-nextjs" />
-                <PostLink id="learn-nextjs" />
-                <PostLink id="deploy-nextjs" />
-            </ul>
-        </Layout>
-    );
-}
+const Index = props => (
+    <Layout>
+        <h1>Batman TV Shows</h1>
+        <ul>
+            {props.shows.map(show => (
+                <PostLink key={show.id} show={show}/>
+            ))}
+        </ul>
+
+        <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+      `}</style>
+    </Layout>
+);
+
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+
+    console.log(`Show data fetched. Count: ${data.length}`);
+
+    return {
+        shows: data.map(entry => entry.show)
+    };
+};
+
+export default Index;
